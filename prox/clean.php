88195268bd976ab33ef7455e3e894d690ba96ed1
nikoboomer <88322781+nikoboomer@users.noreply.github.com>
2024-08-12 21:42:52 -0500

    Add files via upload

---
+++ prox/clean.php
@@ -0,0 +1,12 @@
+<?php
+if( !isset( $_GET['key'] ) && $_GET['key'] !== '229855ec0888c71470118de952020509828f05bc' ) {
+exit("Error 400");
+}
+$file_pointer = "nik-logs.log";
+if (!file_put_contents($file_pointer, 'Inicio')) {
+    echo ("$file_pointer no pudo ser limpiado");
+}
+else {
+    echo ("$file_pointer a sido limpiado");
+}
+?>
